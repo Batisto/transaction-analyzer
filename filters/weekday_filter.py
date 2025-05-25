dd524d1fb1da09c0e3b4b635e0347e74dbcad20d
Maksim Nadyrov <nadyrovmaksim@gmail.com>
2025-05-25 13:43:18 +0300

    uptate project structure

--- filters/weekday_filter.py
+++ filters/weekday_filter.py
@@ -1,12 +1,7 @@
-from transaction import Transaction
 from datetime import datetime
 from typing import List
+from transaction import Transaction
 
-def filter_by_category(transactions: List[Transaction], category: list) -> List[Transaction]:
-    """
-    Фильтрует транзакции по категориям
-    """
-    return [t for t in transactions if t.category == category]
 
 def is_weekday(date: datetime) -> bool:
     """
